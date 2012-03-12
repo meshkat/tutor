e060232066e51b4199e3330ac098dfde3c18f2c1
Meshkat Farr <meshkat@gmail.com>
2012-03-12 14:53:43 -0700

    first one

---
+++ tutor/src/course.java
@@ -0,0 +1,27 @@
+/**
+ * 
+ */
+
+/**
+ * @author Neox
+ *
+ */
+public class course {
+private String teacherName;
+private String courseName;
+
+
+public course(String cName, String tName){
+	courseName=cName;
+	teacherName=tName;	
+}
+public String getCourseName() {
+		return courseName;
+}
+
+public String getTeacherName() {
+	return teacherName;
+}
+
+
+}
